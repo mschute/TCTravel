ab09e30ff93dc4a8cc07a76f1abba096be8df836
mschute <46330682+mschute@users.noreply.github.com>
2024-02-29 17:08:57 +0000

    Added Services Folder
    - Added Services folder and added two new classes, EmailSettings and EmailService

---
+++ TCTravel/Services/EmailService.cs
@@ -0,0 +1,35 @@
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using Microsoft.Extensions.Options;
+using TCTravel.Service;
+
+namespace TCTravel.Services;
+
+
+public class EmailService
+{
+    private readonly EmailSettings _emailSettings;
+
+    public EmailService(IOptions<EmailSettings> emailSettings)
+    {
+        _emailSettings = emailSettings.Value;
+    }
+
+    public void SendEmail(string toEmail, string subject, string body)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("Support CareApp", _emailSettings.SmtpUsername));
+        message.To.Add(new MailboxAddress("Reciever Name", toEmail));
+        message.Subject = subject;
+        var textPart = new TextPart("plain") { Text = body };
+        message.Body = textPart;
+        using (var client = new SmtpClient())
+        {
+            client.Connect(_emailSettings.SmtpServer, _emailSettings.SmtpPort, SecureSocketOptions.StartTls);
+            client.Authenticate(_emailSettings.SmtpUsername, _emailSettings.SmtpPassword);
+            client.Send(message);
+            client.Disconnect(true);
+        }
+    }
+}
