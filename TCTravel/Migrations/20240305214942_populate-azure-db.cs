c0eb58f41717414fc6c976bd182c6e4f18397c8f
mschute <46330682+mschute@users.noreply.github.com>
2024-03-05 22:47:34 +0000

    Connect to database and reconfigure authorisation
    - Remigrate and update database
    - Remove authorisation for roles controller while creating a super admin.

---
+++ TCTravel/Migrations/20240305214942_populate-azure-db.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TCTravel.Migrations
+{
+    /// <inheritdoc />
+    public partial class populateazuredb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
